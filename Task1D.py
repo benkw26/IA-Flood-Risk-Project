6632e54b65e061c66b1aa81c4b180a0e23c01a1e
benkw26 <bensonxkwok@gmail.com>
2022-01-20 12:41:44 +0000

    Task 1D (completed without testing)

--- Task1D.py
+++ Task1D.py
@@ -7,9 +7,14 @@ def run():
     # Build list of stations
     stations = build_station_list()
 
-    # Print number of rivers
-    rivers_with_station(stations)
+    # Print number of rivers with stations along with the list of rivers
+    index = 10 # Optional parameter
+    rivers_with_station(stations, index) 
 
+    # Returns a list of stations corresponding to each river in the list
+    station_testlist = ["River Aire", "River Cam", "River Thames"] # Optional parameter
+    stations_by_river(stations, station_testlist)
+    
 
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part IA Flood Warning System ***")
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -12,20 +12,32 @@ from .utils import sorted_by_key  # noqa
 
 def rivers_with_station(stations, index = 10):
     """Given a list of stations, returns a tuple of (river, monitoring station).
-    Optional parameter index returns the first 'index' number of rivers in alphabetical order"""
+    Optional parameter index: Returns the first 'index' number(s) of rivers in alphabetical order"""
 
     river_list = list(set([station.river for station in stations]))
     river_sortedlist = sorted(river_list)
+    river_station_list = [(station.river, station.name) for station in stations]
     no_of_rivers = len(river_sortedlist)
     if index > no_of_rivers:
         index = 10
     river_displaylist = river_sortedlist[0:index]
-    print(f"{no_of_rivers} stations. The first {index} - {river_displaylist}")
-    return river_sortedlist
+    print(f"{no_of_rivers} stations. The first {index} - {river_displaylist}\n")
+    return river_station_list
 
     
-def stations_by_river(stations):
-    "Given a list of station, returns a dictionary that maps the river names to a list of station objects on the corresponding river"
+def stations_by_river(stations, station_testlist = ["River Aire", "River Cam", "River Thames"]):
+    """Given a list of station, returns a dictionary that maps the river names to a list of station objects on the corresponding river.
+    Optional parameter station_testlist: Prints a list of stations at a river for each river in the list"""
     
-    for station in stations:
-        [].append(station)
+    river_list = list(set([station.river for station in stations]))
+    river_dict = {}
+    for river in river_list:
+        station_list = []
+        for station in stations:
+            if station.river == river:
+                station_list.append(station.name)
+        river_dict[river] = station_list
+    for i in station_testlist:
+        print(f"{river_dict[i]}\n")
+    return river_dict
+            
