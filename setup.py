3acafc60319e78bf0db731caa63190b259f839f8
IdealistMatthew <matthewah1125@gmail.com>
2022-01-20 12:02:04 +0000

    added haverside to workflow dependency installation instead

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup
 setup(name='floodsystem',
       version='0.1',
       description='CUED Part IA flood warning system exercise',
-      libraries=["haversine"],
       packages=['floodsystem'],
       )
