f7167b7fda0fba70472aa32f63debc4fac2d4019
IdealistMatthew <matthewah1125@gmail.com>
2022-01-21 10:52:40 +0000

    Fixed relwaterlvl function and added task2b func

---
+++ test_flood.py
@@ -0,0 +1,12 @@
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_level_over_threshold
+
+def test_stations_level_over_threshold():
+    "Unit test for Task 2B"
+
+    # Build list of stations
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    assert stations_level_over_threshold(stations, 0.8)[0][1] > 0.8
