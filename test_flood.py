0a9735d7fda9a964be424010c4b0902f45b33ba4
benkw26 <bensonxkwok@gmail.com>
2022-01-21 12:06:31 +0000

    unit test

--- test_flood.py
+++ test_flood.py
@@ -1,2 +1,11 @@
-from floodsystem.flood import *
+"""Unit test for the flood module"""
+from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list
+
+def test_stations_highest_rel_level():
+    "Unit test for Task 2C"
+    stations = build_station_list()
+    N = 5
+    for i in range(N):
+        assert stations_highest_rel_level(stations, N)[i][1] >= stations_highest_rel_level(stations, N)[i+1][1]
+
