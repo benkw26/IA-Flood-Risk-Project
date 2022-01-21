9bb671e00964bc80463d936c64b1dd8a084b47fd
benkw26 <bensonxkwok@gmail.com>
2022-01-21 10:36:48 +0000

    added documentation for flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,5 +1,18 @@
 
 def stations_highest_rel_level(stations, N):
+    """
+    returns the stations with the highest relative water level
+
+    Inputs
+    ------
+    stations: list of MonitoringStation objects
+    N: int, the number of stations to return
+
+    Returns
+    ------
+    returns the N stations that have the highest relative water level in descending order
+    """
+    
     station_level_list = stations_level_over_threshold(stations, 0.0)
     return station_level_list[:N]
 
