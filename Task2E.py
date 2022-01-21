ef54f95d758c601fce6eb763d675b823cae79a71
IdealistMatthew <matthewah1125@gmail.com>
2022-01-21 11:51:43 +0000

    Complete Task2E

---
+++ Task2E.py
@@ -0,0 +1,24 @@
+import datetime
+
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_levels
+
+
+def run():
+    """Requirements for Task2E"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    stat = stations_highest_rel_level(stations,5)
+
+    for station in stat:
+        dt = 10
+        dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
