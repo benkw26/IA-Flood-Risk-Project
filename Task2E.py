f01f01bbe14745e3baca70e3ac41a1c236a70881
IdealistMatthew <matthewah1125@gmail.com>
2022-01-21 11:52:54 +0000

    Added import for function needed

--- Task2E.py
+++ Task2E.py
@@ -3,6 +3,7 @@ import datetime
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_highest_rel_level
 
 
 def run():
